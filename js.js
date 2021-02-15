cd6d1605ad92af1f938e3d8c14ff3e8cbc15fd13
Vynter <amine-cheraitia@hotmail.com>
2021-02-15 14:26:54 +0100

    2nd k

--- js.js
+++ js.js
@@ -28,3 +28,99 @@ img.addEventListener("dblclick", function () {
     title.innerText = change;
   }
 });
+
+/*-----------------------exo 2----------------------- */
+
+var body = document.querySelector("body");
+var tab = document.createElement("table");
+var cl = document.createElement("caption");
+cl.innerText = "Table de multuplication dynamique";
+tab.appendChild(cl);
+body.append(tab);
+
+var max = 18;
+var min = 12;
+
+var tagHTr = document.createElement("tr");
+
+for (let i = min; i <= max; i++) {
+  let tagTd = document.createElement("td");
+  resultat = i;
+  tagTd.innerHTML = resultat;
+  if (resultat % 10 === 0) {
+    tagTd.style.backgroundColor = "pink";
+  } else if (resultat % 5 === 0) {
+    tagTd.style.backgroundColor = "yellow";
+  } else if (resultat % 2 === 0) {
+    tagTd.style.backgroundColor = "orange";
+  }
+  tagHTr.appendChild(tagTd);
+  tab.appendChild(tagHTr);
+}
+
+for (let k = min + 1; k <= max; k++) {
+  let tagTr = document.createElement("tr");
+  let tagTd = document.createElement("td");
+  resultat = k;
+  tagTd.innerHTML = resultat;
+  if (resultat % 10 === 0) {
+    tagTd.style.backgroundColor = "pink";
+  } else if (resultat % 5 === 0) {
+    tagTd.style.backgroundColor = "yellow";
+  } else if (resultat % 2 === 0) {
+    tagTd.style.backgroundColor = "orange";
+  }
+  tagTr.appendChild(tagTd);
+
+  for (let i = min + 1; i <= max; i++) {
+    let tagTd = document.createElement("td");
+
+    let resultat = k * i;
+    tagTd.innerHTML = resultat;
+    if (resultat % 10 === 0) {
+      tagTd.style.backgroundColor = "pink";
+    } else if (resultat % 5 === 0) {
+      tagTd.style.backgroundColor = "yellow";
+    } else if (resultat % 2 === 0) {
+      tagTd.style.backgroundColor = "orange";
+    }
+    tagTr.appendChild(tagTd);
+    tab.appendChild(tagTr);
+  }
+}
+
+var a = 1; /*
+  }
+}
+*/
+
+/*var arr = new Array(10, 10);
+
+for (var i = 1; i <= 10; i++) {
+  for (var k = 1; k <= 10; k++) {
+    arr[i][k] = k;
+  }
+}
+
+console.log(arr);
+*/
+/*
+for (var i = min; i <= max; i++) {
+  //console.log(i);
+  var tagTr = document.createElement("tr");
+  for (var j = min; j <= max; j++) {
+    let tagTd = document.createElement("td");
+
+    let resultat = j * i;
+    tagTd.innerHTML = resultat;
+    if (resultat % 10 === 0) {
+      tagTd.style.backgroundColor = "pink";
+    } else if (resultat % 5 === 0) {
+      tagTd.style.backgroundColor = "yellow";
+    } else if (resultat % 2 === 0) {
+      tagTd.style.backgroundColor = "orange";
+    }
+    tagTr.appendChild(tagTd);
+    tab.appendChild(tagTr);
+    /*arr[i][j].push(j);
+    console.log(arr[i] + " " + arr[j]);*/
