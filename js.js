c07f65c40a7ad17384dc3a1938170f267e12429b
Vynter <amine-cheraitia@hotmail.com>
2021-02-15 11:29:34 +0100

    first ok

--- js.js
+++ js.js
@@ -2,15 +2,23 @@ var img = document.querySelector("img");
 var title = document.querySelector("h2");
 var imgTab = img.style.width.split("");
 var imgWith = parseInt(imgTab.slice(0, imgTab.length - 2).join(""));
-console.log(imgWith * 3);
+//console.log(imgWith * 3);
+var w = img.width;
+var h = img.height;
+console.log(w * 3);
 
 img.addEventListener("mouseenter", function () {
-  this.style.width = imgWith * 3 + "px";
-  console.log(this.style.width);
+  /*this.style.width = imgWith * 3 + "px";
+  console.log(this.style.width);*/
+  this.width = w * 3;
+  this.height = h * 3;
+  console.log(this.width);
 });
 
 img.addEventListener("mouseleave", function () {
-  this.style.width = imgWith + "px";
+  //this.style.width = imgWith + "px";
+  this.width = w;
+  this.height = h;
 });
 
 img.addEventListener("dblclick", function () {
