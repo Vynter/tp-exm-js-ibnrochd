9213aedd5b2fee76f3b1c63574b47e01c47784f5
Vynter <amine-cheraitia@hotmail.com>
2021-02-15 18:54:36 +0100

    cr

--- js.js
+++ js.js
@@ -6,6 +6,7 @@ var imgWith = parseInt(imgTab.slice(0, imgTab.length - 2).join(""));
 var w = img.width;
 var h = img.height;
 console.log(w * 3);
+var body = document.querySelector("body");
 
 img.addEventListener("mouseenter", function () {
   /*this.style.width = imgWith * 3 + "px";
@@ -31,7 +32,6 @@ img.addEventListener("dblclick", function () {
 
 /*-----------------------exo 2----------------------- */
 function TabMulti(min = 0, max = 12) {
-  var body = document.querySelector("body");
   //var br = document.querySelector("br");
   var tab = document.createElement("table");
   var cl = document.createElement("caption");
@@ -134,7 +134,9 @@ function TabMulti(min = 0, max = 12) {
           let maxA = parseInt(maxV);
           tab.parentNode.removeChild(tab);
           form.parentNode.removeChild(form);
+          body.removeChild(document.querySelector("#form2"));
           TabMulti(minA, maxA);
+          pgcdCreate();
         }
       }
     } else {
@@ -145,3 +147,41 @@ function TabMulti(min = 0, max = 12) {
   });
 }
 TabMulti();
+
+function pgcdCreate() {
+  let form = document.createElement("form");
+  form.setAttribute("id", "form2");
+  let l1 = document.createElement("label");
+  l1.setAttribute("for", "a");
+  l1.innerText = "A=";
+  let inp1 = document.createElement("input");
+  inp1.setAttribute("type", "text");
+  inp1.setAttribute("id", "a");
+  l1.appendChild(inp1);
+  form.appendChild(l1);
+  let l2 = document.createElement("label");
+  l2.setAttribute("for", "b");
+  l2.innerText = "B=";
+  let inp2 = document.createElement("input");
+  inp2.setAttribute("type", "text");
+  inp2.setAttribute("id", "b");
+  l2.appendChild(inp2);
+  let bt = document.createElement("button");
+  bt.setAttribute("type", "submit");
+  bt.setAttribute("id", "calc");
+  bt.innerHTML = "Calcule PGCD";
+  let l3 = document.createElement("label");
+  l3.setAttribute("for", "res");
+  l3.innerText = "Resultat=";
+  let inp3 = document.createElement("input");
+  inp3.setAttribute("type", "text");
+  inp3.setAttribute("id", "res");
+  l3.appendChild(inp3);
+  form.appendChild(l1);
+  form.appendChild(l2);
+  form.appendChild(l3);
+  form.appendChild(bt);
+  body.appendChild(form);
+}
+
+pgcdCreate();
